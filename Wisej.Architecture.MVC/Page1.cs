d46f62f4453d42bd0e236b404811352b2c124dad
JulieBodette <jr3python@gmail.com>
2023-06-26 17:05:24 -0400

    Wisej.Architecture.MVC initial commit

---
+++ Wisej.Architecture.MVC/Page1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Wisej.Web;
+
+namespace Wisej.Architecture.MVC
+{
+	public partial class Page1 : Page
+	{
+		List<StudentModel> studentdata = new List<StudentModel>();
+		public Page1()
+		{
+			InitializeComponent();
+		}
+
+		/// Load data from the database on initial page load.
+		private void Page1_Load(object sender, System.EventArgs e)
+		{
+			studentdata = StudentModel.GetStudents();
+			dataGridView1.DataSource = studentdata;
+		}
+
+		/// Add a new student to the database using the values from the text fields.
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			//check to make sure that all the text fields have text (ie they are not the empty string)
+			if (txtId.Text != "" && txtEmail.Text != "" && txtName.Text != "" && txtAge.Text != "")
+			{
+				//read the data from the text fields in the view
+				int id = Int32.Parse(txtId.Text);
+				string email = txtEmail.Text;
+				string name = txtName.Text;
+				int age = Int32.Parse(txtAge.Text);
+
+				// create a StudentModel based on the data in the text fields.
+				StudentModel model = new StudentModel() { Name = name, Id = id, Age = age, Email = email };
+				// Attempt to add the student to the database- show a sucess or failure message.
+				string message = model.AddStudent();
+				AlertBox.Show(message);
+
+				//clear the textboxes
+				txtId.Text = "";
+				txtEmail.Text = "";
+				txtName.Text = "";
+				txtAge.Text = "";
+
+				//Get the data from the database and show it in the view so that the new student is seen
+				studentdata = StudentModel.GetStudents();
+				dataGridView1.DataSource = studentdata;
+			}
+			else //if at least one of the text fields is blank
+			{
+				AlertBox.Show("Please enter values for all fields.");
+			}
+		}
+	}
+}
---
+++ Wisej.Architecture.MVC/StudentModel.cs
@@ -0,0 +1,88 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+
+namespace Wisej.Architecture.MVC
+{
+	public class StudentModel
+	{
+		[Range(1, 999, ErrorMessage = "Id must be between 1 and 999")]
+		public int Id { get; set; }
+
+		[EmailAddress(ErrorMessage = "Invalid Email Address")]
+		public string Email { get; set; }
+
+		public string Name { get; set; }
+		public int Age { get; set; }
+
+		// Returns a string either indicating that the given StudentModel is valid, or with an error message explaining which part(s) is invalid
+		static public string ValidateData(StudentModel model, string validmessage)
+		{
+			string message = "";
+			ValidationContext context = new ValidationContext(model, null, null);
+			List<ValidationResult> validationResults = new List<ValidationResult>();
+			bool valid = Validator.TryValidateObject(model, context, validationResults, true);
+			if (!valid)
+			{
+				foreach (ValidationResult validationResult in
+				validationResults)
+				{
+					message += validationResult.ErrorMessage + " \n";
+				}
+			}
+			else
+			{
+				message = validmessage;
+			}
+			return message;
+
+		}
+
+		// Looks up the database connection string. The database connection string is stored in Web.config.
+		public static string CnnVal(string name)
+		{
+			return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+		}
+
+		// returns a list of StudentModel objects from the database
+		public static List<StudentModel> GetStudents()
+		{
+			using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(StudentModel.CnnVal("Students")))
+			{
+				var output = connection.Query<StudentModel>("select * from Students").ToList();
+				return output;
+			}
+		}
+
+		// Adds the model to the database. Returns a string which contains an error message if the model is invalid.
+		public string AddStudent()
+		{
+			string validMessage = "Added new student to the database.";
+
+			// ValidateData returns a string with the validation errors. Returns validMessage if the data is valid.
+			string message = StudentModel.ValidateData(this, validMessage);
+
+			// if the data in the model is valid, add the student to the database
+			if (message == validMessage)
+			{
+
+				//add the data to the database
+				using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(StudentModel.CnnVal("Students")))
+				{
+					connection.Execute("INSERT INTO Students VALUES (@Id, @Email, @Name, @Age)", this);
+					//note: The order and exact case-sensitive text of the values @Id, @Email, @Name, @Age MUST match the database
+				}
+			}
+			// If the data is not valid, add a bit to the message informing the user that the student was not added to the database.
+            else
+            {
+				message += " New student was not added to database.";
+            }
+
+            return message;
+		}
+	}
+}
